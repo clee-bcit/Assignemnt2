7191f21dc46809fe03ac91d2acab1e5883c9b49b
Chris Lee <jincana96@gmail.com>
2021-11-21 17:23:03 -0800

    completed javacript for view page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Index Page //
+/* Index Page */
 function cleanUpIndex() {
     const cleanUp = document.querySelectorAll('.contact');
     cleanUp.remove;
@@ -20,7 +20,7 @@ let contactList = [
     },
   ]
 
-function createSingleIndex() {
+function createSingleIndex(contact) {
     let contactList = [
         {
           name: "Roberta Dobbs",
@@ -41,7 +41,7 @@ function createSingleIndex() {
     const divNode = document.createElement('div');
     const pTag = document.createElement('p');
 
-    const textNode = document.createTextNode(contactList.name)
+    const textNode = document.createTextNode(contact.name)
     
 
 
@@ -89,4 +89,100 @@ function renderIndex() {
     };
 }
 
-// View Page //
+/* View Page */
+function cleanUpView() {
+    const cleanUp = document.querySelectorAll('.contactinfo');
+    cleanUp.remove;
+    for(let i = 0; i < cleanUp.length; i++) { cleanUp[i].remove();}
+}
+
+function renderView(contact) {
+    const main = document.querySelectorAll('.main');
+    const divContactinfoNode = document.createElement('div');
+    const divConstactnameNode = document.createElement('div')
+    const divContactemailNode = document.createElement('div')
+    const divContactphoneNode = document.createElement('div')
+    const divContactaddressNode = document.createElement('div')
+    const divButtonsNode = document.createElement('div')
+    const nameNode = document.createTextNode(contact.name);
+    const imgNode = document.createElement('img');
+    imgNode.src = "./img/profile.jpg"
+    const emailNode = document.createTextNode(contact.email);
+    const phoneNode = document.createTextNode(contact.phone);
+    const addressNode = document.createTextNode(contact.address);
+    const editNode = document.createElement('button');
+    const editButton = document.createTextNode('Edit')
+    const closeNode = document.createElement('button');
+    const closeButton = document.createTextNode('Close');
+
+
+    divContactinfoNode.className = 'contactinfo'
+
+    divConstactnameNode.className = 'contactname';
+    divConstactnameNode.appendChild(nameNode);
+    divConstactnameNode.append(nameNode);
+    divConstactnameNode.appendChild(imgNode); 
+    imgNode.className = 'profilepic'
+    divContactinfoNode.appendChild(divConstactnameNode);
+
+    divContactemailNode.className = 'contactemail';
+    divContactemailNode.appendChild(emailNode);
+    divContactinfoNode.appendChild(divContactemailNode);
+
+    divContactphoneNode.className = 'contactphone';
+    divContactphoneNode.appendChild(phoneNode);
+    divContactinfoNode.appendChild(divContactphoneNode);
+
+    divContactaddressNode.className = 'contactaddress';
+    divContactaddressNode.appendChild(addressNode);
+    divContactinfoNode.appendChild(divContactaddressNode);
+
+    divButtonsNode.className = 'buttons';
+    divButtonsNode.appendChild(editNode)
+    editNode.appendChild(editButton);
+    editNode.className = 'button edit';
+    divButtonsNode.appendChild(closeNode);
+    closeNode.appendChild(closeButton);
+    closeNode.className = 'button close';
+    divContactinfoNode.appendChild(divButtonsNode);
+
+    main[0].appendChild(divContactinfoNode);
+}
+
+/* Create Page */
+
+function cleanUpCreate() {
+    const cleanUp = document.querySelectorAll('.contactedit');
+    cleanUp.remove;
+    for(let i = 0; i < cleanUp.length; i++) { cleanUp[i].remove();}
+}
+
+function renderCreate() {
+    const main = document.querySelectorAll('.main');
+    const divContactedit = document.createElement('div');
+    const divContactimg = document.createElement('div');
+    const divForm = document.createElement('div');
+    const divInputName = document.createElement('div');
+    const divInputPhone = document.createElement('div');
+    const divInputAddress = document.createElement('div');
+    const divInputEmail = document.createElement('div');
+    const divInputButtons = document.createElement('div');
+
+    const imgNode = document.createElement('img');
+    imgNode.src = "./img/profile.jpg"
+
+    const formNode = document.createElement('form');
+
+    const inputName = document.createElement('input')
+    const inputPhone = document.createElement('input')
+    const inputAddress = document.createElement('input')
+    const inputEmail = document.createElement('input')
+
+    const buttonName = document.createElement('button')
+    const buttonPhone = document.createElement('button')
+    const buttonAddress = document.createElement('button')
+    const buttonEmail = document.createElement('button')
+    const buttonSave = document.createElement('button')
+    const buttonCancel = document.createElement('button')
+
+}
